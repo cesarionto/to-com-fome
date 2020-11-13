35b5b2cfaaac5d2a95a7f8644a3eed1937460ac3
cesarionto <cesariopereiraneto@gmail.com>
2020-11-13 08:50:13 -0300

    Initial commit

---
+++ src/main/java/com/cesario/estagio/ufersa/tocomfomeapi/ToComFomeApiApplication.java
@@ -0,0 +1,13 @@
+package com.cesario.estagio.ufersa.tocomfomeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToComFomeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToComFomeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cesario/estagio/ufersa/tocomfomeapi/ToComFomeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesario.estagio.ufersa.tocomfomeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToComFomeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
