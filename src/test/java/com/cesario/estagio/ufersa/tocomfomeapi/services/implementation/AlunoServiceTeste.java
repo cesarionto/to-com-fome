5780a290f4987aad7e4be5fc733f7afa30c05bd3
cesarionto <cesariopereiraneto@gmail.com>
2020-11-14 21:23:07 -0300

    aplicando testes de unidade nas funcionalidades já desenvolvidas.

---
+++ src/test/java/com/cesario/estagio/ufersa/tocomfomeapi/services/implementation/AlunoServiceTeste.java
@@ -0,0 +1,44 @@
+package com.cesario.estagio.ufersa.tocomfomeapi.services.implementation;
+
+import com.cesario.estagio.ufersa.tocomfomeapi.domain.models.Aluno;
+import com.cesario.estagio.ufersa.tocomfomeapi.domain.repository.AlunoRepository;
+import com.cesario.estagio.ufersa.tocomfomeapi.domain.services.exceptions.RegraNegocioException;
+import com.cesario.estagio.ufersa.tocomfomeapi.domain.services.implementation.AlunoService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+public class AlunoServiceTeste {
+
+    @Autowired
+    AlunoService alunoService;
+
+    @Autowired
+    AlunoRepository alunoRepository;
+
+    @Test
+    public void deveValidarMatricula(){
+        alunoRepository.deleteAll();
+        alunoService.validarMatricula(2016021154);
+    }
+
+    @Test
+    public void deveLancarExceptionAoValidarMatricula() throws RegraNegocioException{
+        Aluno aluno = new Aluno();
+        aluno.setNomeCompleto("Cesário Pereira Neto");
+        aluno.setMatricula(2016021154);
+        alunoRepository.save(aluno);
+        Exception exception = assertThrows(RegraNegocioException.class,
+                ()-> {
+                    alunoService.validarMatricula(2016021154);
+                });
+    }
+}
